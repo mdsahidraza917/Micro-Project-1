93fc8e71f3b5afcb5d93eefa736cda610049b806
Md Sahid Raza <89832304+mdsahidraza917@users.noreply.github.com>
2024-11-08 23:54:18 +0530

    Group Creation using Modal Done

---
+++ SUPERAPP/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {RouterProvider,} from "react-router-dom";
+import App from './App.jsx'
+import './index.css'
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router = {router}>
+  </StrictMode>,
+)
